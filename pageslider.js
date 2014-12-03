cdde6202c7587965a2b780d45d8545e1e6c649a5
Michael Dobekidis <m.dobekidis@7linternational.com>
2014-12-03 12:34:14 +0200

    pageSlider now supports all animate.css animations
    pageSlider now supports all animate.css animations

--- pageslider.js
+++ pageslider.js
@@ -10,39 +10,46 @@ function PageSlider(container) {
         stateHistory = [];
 
     // Use this function if you want PageSlider to automatically determine the sliding direction based on the state history
-    this.slidePage = function(page) {
+    this.slidePage = function(page, mode) {
 
         var l = stateHistory.length,
             state = window.location.hash;
 
+        var _mode = mode || "fadeOutRight";
+
         if (l === 0) {
             stateHistory.push(state);
-            this.slidePageFrom(page);
+            this.slidePageFrom(page, _mode);
             return;
         }
         if (state === stateHistory[l-2]) {
             stateHistory.pop();
-            this.slidePageFrom(page, 'left');
+            this.slidePageFrom(page, "fadeOutRight", "fadeInLeft");
         } else {
             stateHistory.push(state);
-            this.slidePageFrom(page, 'right');
+            this.slidePageFrom(page, "fadeOutLeft", "fadeInRight");
         }
 
     };
 
     // Use this function directly if you want to control the sliding direction outside PageSlider
-    this.slidePageFrom = function(page, from) {
+    this.slidePageFrom = function(page, out, animationIn) {
 
         container.append(page);
 
-        if (!currentPage || !from) {
+        if(animationIn === undefined || animationIn === null) {
+            animationIn = "fadeInRight";
+        }
+
+        if (!currentPage || !out) {
+            window.console.log("no page");
             page.attr("class", "page center");
             currentPage = page;
             return;
         }
 
         // Position the page at the starting position of the animation
-        page.attr("class", "page " + from);
+        page.attr("class", "page " + out);
 
         currentPage.one('webkitTransitionEnd', function(e) {
             var mt = $(e.target);
@@ -51,7 +58,7 @@ function PageSlider(container) {
                 mt.remove();
             };
         });
-        
+
         currentPage.one('webkitAnimationEnd', function(e) {
             var mt = $(e.target);
             // If the user presses back while the animation is in progress, this prevents it from breaking since the app changes classes before 'webkitTransitionEnd'
@@ -64,8 +71,8 @@ function PageSlider(container) {
         container[0].offsetWidth;
 
         // Position the new page and the current page at the ending position of their animation with a transition class indicating the duration of the animation
-        page.attr("class", "page transition center");
-        currentPage.attr("class", "page transition " + (from === "left" ? "right" : "left"));
+        page.attr("class", "page transition animated " + animationIn );
+        currentPage.attr("class", "page transition animated " + (out === "left" ? "left" : out));
         currentPage = page;
     };
 
